d08f73490d31099a6ecba70f1c0c7cdf094d1e29
Jian <JianPitpit@gmail.com>
2024-12-14 22:02:58 -0700

    Added all necessary classes

---
+++ OOPFinal/OOPFinal/DBManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFinal.Platforms
+{
+    internal class DBManager
+    {
+    }
+}
--- OOPFinal/OOPFinal/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/OOPFinal.AssemblyInfo.cs
+++ OOPFinal/OOPFinal/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/OOPFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOPFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+35fa58f4ca1d392a499a087aa38d5748fa504f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+631d3884cc51fc2048fd057dbee1b4d9aed3fa67")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOPFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOPFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
